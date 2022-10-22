b30f0ab14a0f34c7ba50e797ee251daa092c150d
maevskiEd <maevski.ed@gmail.com>
2022-10-22 08:46:15 +0300

    t20_5_1

--- app/src/main/java/ed/maevski/p20/MainActivity.kt
+++ app/src/main/java/ed/maevski/p20/MainActivity.kt
@@ -2,10 +2,18 @@ package ed.maevski.p20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonKosmos = findViewById<Button>(R.id.buttonKosmos)
+
+        buttonKosmos.setOnClickListener {
+            Toast.makeText(this, "Я буду android-разработчиком!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
